e9bdcde6e0c7b8a80bc3202ede01a8bcb865012d
Charmi Hemant Kumar Shah <cshah@evoketechnologies.com>
2023-03-15 13:16:18 +0530

    app and style css put back as a raw and sidebar css updated

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,12 +1,11 @@
 <template>
-    <q-layout view="hHh Lpr lff" container style="height: 300px" class="shadow-2 rounded-borders hcus">
+    <q-layout view="hHh Lpr lff" container style="height: 500px" class="shadow-2 rounded-borders">
       <q-drawer
         v-model="drawer"
         show-if-above
-        :width="200"
+        :width="230"
         :breakpoint="500"
         bordered
-        class="bg-grey-3"
       >
         <q-scroll-area class="fit">
           <q-list>
@@ -15,33 +14,43 @@
               <q-item  v-ripple>
                 
                 <q-item-section>
-                  <details>
-                    <summary>{{ menuItem.label }}</summary>
-                    <p v-for="(menuSubItem, index) in menuItem.listOfSubCom" :key="index">
-                       <RouterLink :to="menuSubItem.path">{{ menuSubItem.name }}</RouterLink>
-                    </p>
-                   
-                  </details>
+                  <section>
+                    <details class="details" >
+                      <summary class="summary">{{ menuItem.label }}</summary>
+                      <details class="details">
+                       
+                        <summary class="summary summary1">
+                          <p v-for="(menuSubItem, index) in menuItem.listOfSubCom" :key="index">
+                            <RouterLink :to="menuSubItem.path">{{ menuSubItem.name }}</RouterLink>
+                          </p>
+                         
+                        </summary>
+                      </details>
+                    </details>
+
+                  </section>
                 </q-item-section>
               </q-item>
-              <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
+              <!-- <q-separator :key="'sep' + index"  v-if="menuItem.separator" /> -->
             </template>
 
           </q-list>
         </q-scroll-area>
       </q-drawer>
-
       <q-page-container>
         <q-page padding>
           <RouterView />
         </q-page>
       </q-page-container>
+     
     </q-layout>
 </template>
 <script>
 import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import {menuList} from '../utils/menuList'
+import '../css/styles.css';
+import './SideBar.css';
 
 console.log("menulist from utils",menuList);
 
@@ -57,3 +66,16 @@ export default {
 }
 
 </script>
+<style>
+
+.side-bar {
+  width: 100% !important;
+  overflow: hidden !important;
+}
+.sideBar {
+  width: 100%;
+}
+section > .details > .summary {
+  padding: 10px 15px 10px 25px !important;
+}
+</style>
